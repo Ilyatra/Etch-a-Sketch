8b1c83937c4d1e4013374e42e88767be9849e555
Marvin <marvin996@gmail.com>
2021-08-02 00:28:06 +0500

    add colorize and random colorize

--- js.js
+++ js.js
@@ -5,7 +5,6 @@ const createCanvas = function createCanvas(squaresInLine) {
     let canvas = [];
     const corrections = 10; /* из-за потери точности пришлось добавить корректировку в 10px 
                                 и flex-shrink для square */
-    
     const squareHeight = (parseInt(getComputedStyle(canvasWrapper).height) + corrections )/ squaresInLine + 'px';
     const squareWidth = (parseInt(getComputedStyle(canvasWrapper).width) + corrections )/ squaresInLine + 'px';
 
@@ -17,16 +16,30 @@ const createCanvas = function createCanvas(squaresInLine) {
         }
         canvas.push(row);
     }
-    
     return canvas;
 }
 
 const createSquare = function createSquare(height, width) {
     let square = document.createElement('div');
-    square.classList.add('red');
+    square.classList.add('canvas__square');
     square.style.height = height;
     square.style.width = width;
     return square;
 }
+
+const colorize = function colorizeSquare (target, color) {
+    target.style.backgroundColor = `rgb(${color})`;
+} 
+
+const randomizeColor = function randomizeColor () {
+    let rgb = [0,0,0];
+    return rgb.map(() =>  Math.floor(Math.random() * 255) + 1).join(',');
+}
+
+canvasWrapper.addEventListener('mouseover',(e) => {
+    if (!e.target.classList.contains('canvas__square')) return;
+    colorize(e.target, randomizeColor());
+    }
+)
 
 canvasWrapper.append(...createCanvas(90));
